bf305cd0963fd0a16d0c179a2ec1c83a4ee5d86d
goliatone <burgosemi@gmail.com>
2018-06-25 14:28:19 -0400

    add index

---
+++ index.js
@@ -0,0 +1,9 @@
+module.exports.Storage = require('./lib/storage');
+
+module.exports.Watcher = require('./lib/watcher');
+
+module.exports.Signals = require('./lib/signals');
+
+module.exports.ChangeTypes = require('./lib/change-type');
+
+module.exports.Subscription = require('./lib/subscription');
